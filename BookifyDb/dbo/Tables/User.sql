8565743925298016fdaab7ea706ab11612a06681
Tomasz <urban776@gmail.com>
2020-10-21 20:33:41 +0200

    Add Database project with User table and DataLibrary project

---
+++ BookifyDb/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL,
+)
